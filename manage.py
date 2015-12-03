2763700f32ce4d5479a68252b384a106acae2770
cruor99 <kliknes@gmail.com>
2015-12-03 23:31:12 +0100

    initial commit

---
+++ manage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+
+from flask.ext.script import Manager, Server
+from flask.ext.script.commands import ShowUrls, Clean
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+from neogameserver import create_app
+from neogameserver.models import db, User
+
+# default to dev config because no one should use this in
+# production anyway
+env = os.environ.get('neogameserver_ENV', 'dev')
+app = create_app('neogameserver.settings.%sConfig' % env.capitalize(), env=env)
+
+manager = Manager(app)
+manager.add_command("server", Server())
+manager.add_command("show-urls", ShowUrls())
+manager.add_command("clean", Clean())
+manager.add_command("db", MigrateCommand)
+
+migrate = Migrate(app, db)
+
+@manager.shell
+def make_shell_context():
+    """ Creates a python REPL with several default imports
+        in the context of the app
+    """
+
+    return dict(app=app, db=db, User=User)
+
+
+@manager.command
+def createdb():
+    """ Creates a database with all of the tables defined in
+        your SQLAlchemy models
+    """
+
+    db.create_all()
+
+if __name__ == "__main__":
+    manager.run()
---
+++ neogameserver/models.py
@@ -0,0 +1,46 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import UserMixin, AnonymousUserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    email = db.Column(db.String())
+    role = db.Column(db.String())
+
+    def __init__(self, username, password, email, role):
+        self.username = username
+        self.set_password(password)
+        self.email = email
+        self.role = role
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, value):
+        return check_password_hash(self.password, value)
+
+    def is_authenticated(self):
+        if isinstance(self, AnonymousUserMixin):
+            return False
+        else:
+            return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        if isinstance(self, AnonymousUserMixin):
+            return True
+        else:
+            return False
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return '<User %r>' % self.username
